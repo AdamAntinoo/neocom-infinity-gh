7aa72dc5f9750f72f4f5b5fbd41a56603bb329ba
Adam Antinoo <adamantinoo.git@gmail.com>
2017-08-04 12:13:54 +0200

    -Added the return information form the backend to the pilot list. Now the pilot show some of the information.
    Former-commit-id: 8e7cb0db127053cad9779b60dd5a8a39d1b96fe2

--- Neocom.WebUI/src/app/models/pilot.ts
+++ Neocom.WebUI/src/app/models/pilot.ts
@@ -1,23 +1,39 @@
-// PROJECT:     POC-ASB-Planetary (POC.ASBP)
+// PROJECT:     NEOCOM.WEB (NEOC.W)
 // AUTHORS:     Adam Antinoo - adamantinoo.git@gmail.com
 // COPYRIGHT:   (c) 2017 by Dimensinfin Industries, all rights reserved.
 // ENVIRONMENT: Angular - CLASS
-// DESCRIPTION: Defines the structure for a list of resources. Basically the id and the name of the list and possible it can be extended to contain the list of associated resources.
-export class Pilot {
-  public id: number = 0.0;
-  public name: string = "-Default-";
+// DESCRIPTION: Defines the structure of a EVE Pilot. May depend on other classes to complete the character information hierarchy.
+
+//--- MODELS
+import { NeocomNode }   from './neocomnode';
+import { PilotAction }  from './pilotaction';
+
+export class Pilot extends NeocomNode {
+  public characterID: number = -1.0;
+  public active: boolean = true;
+  public accountBalance: number = -1.0;
+  public urlforAvatar: string = "http://image.eveonline.com/character/92223647_256.jpg";
+  public lastKnownLocation: string = "- HOME -";
+  public name: string = "<name>";
+  public actions: PilotAction[] = [];
+  public corporation: boolean = false;
+
 
   constructor(values: Object = {}) {
+    super();
     Object.assign(this, values);
   }
 
   public getId() {
-    return this.id;
+    return this.characterID;
   }
   public getName() {
     return this.name;
   }
   public setName(newname: string) {
     this.name = newname;
+  }
+  public getUrlforAvatar() {
+    return this.urlforAvatar;
   }
 }
--- Neocom.WebUI/src/app/page/page-pilots/page-pilots.component.ts
+++ Neocom.WebUI/src/app/page/page-pilots/page-pilots.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }  from '@angular/core';
+import { CookieService }      from 'ngx-cookie';
+
+//--- SERVICES
+import { PilotRoasterService } from '../../services/pilot-roaster.service';
+//--- MODELS
+import { Pilot } from '../../models/Pilot';
 
 @Component({
   selector: 'neocom-page-pilots',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-pilots.component.css']
 })
 export class PagePilotsComponent implements OnInit {
+  public pilotList: Pilot[] = [];
 
-  constructor() { }
+  constructor(private pilotRoasterService: PilotRoasterService, private _cookieService: CookieService) { }
 
   ngOnInit() {
+    // Download the list fo pilots from the backend service.
+    console.log("--[PagePilotsComponent.ngOnInit]> firing getPilotRoaster");
+    // Define the cookie and initialize it to the login predefined value.
+    this._cookieService.put("login-id", "default")
+    this.pilotRoasterService.getAllPilots()
+      .subscribe(result => {
+        console.log("--[PagePilotsComponent.ngOnInit.subscribe]> pilot list: " + JSON.stringify(result));
+        // The the list of planatary resource lists to the data returned.
+        this.pilotList = result;
+      });
   }
-
+  public collaborate2Model() {
+    return this.pilotList;
+  }
 }
