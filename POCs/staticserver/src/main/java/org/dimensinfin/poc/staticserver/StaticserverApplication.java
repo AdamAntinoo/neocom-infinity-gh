1b5de93d9722123a47549b0abeaeeed80fd48317
Adam Antinoo <adamantinoo.git@gmail.com>
2019-08-30 22:02:23 +0200

    -[POC] New project to check the static content development integrated with the REST background and Angular development.
    Former-commit-id: 43dcdf03e94ea86f9b0bfb80e18a8e89ebe37893

---
+++ POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
@@ -0,0 +1,13 @@
+package org.dimensinfin.poc.staticserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticserverApplication.class, args);
+	}
+
+}
