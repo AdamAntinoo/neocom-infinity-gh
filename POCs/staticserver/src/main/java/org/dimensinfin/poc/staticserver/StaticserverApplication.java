0ffa0524dc78f11174cfa8cf7394a442fccde7b2
Adam Antinoo <adamantinoo.git@gmail.com>
2019-08-31 16:48:34 +0200

    -[POC.STATICSERVER] Configuration to adapt to IntelliJ
    Former-commit-id: 6a718acbcd10da57d2309a8bcb028f1175b5ce23

--- POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
+++ POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StaticserverApplication {
 
-	public static void main(String[] args) {
+	public static void main( String[] args ) {
 		SpringApplication.run(StaticserverApplication.class, args);
 	}
 
