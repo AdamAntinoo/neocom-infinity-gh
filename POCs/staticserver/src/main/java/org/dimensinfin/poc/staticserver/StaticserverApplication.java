25bf38a7fc36c7debbfa834abf3bdf9cc8eaad25
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-01 15:17:31 +0200

    -[POC.STATICSERVER] Added some code but still the application does not serve the static content on standard run.
    Former-commit-id: 7bd6c633b36f67c55eafc09fdcaa56e731d05fd9

--- POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
+++ POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
@@ -2,9 +2,10 @@ package org.dimensinfin.poc.staticserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StaticserverApplication {
+public class StaticserverApplication extends SpringBootServletInitializer {
 
 	public static void main( String[] args ) {
 		SpringApplication.run(StaticserverApplication.class, args);
