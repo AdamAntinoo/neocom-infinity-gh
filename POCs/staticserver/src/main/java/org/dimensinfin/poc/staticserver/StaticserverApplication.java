97fbb0f9de38c3a2a899d8afd397d881fe1a7d0d
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-02 10:47:49 +0200

    -[POC.STATICSERVER] Server still does not recognizes static content. Restarting.
    Former-commit-id: be116b0ed9df6518966f76e55943200a24746234

--- POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
+++ POCs/staticserver/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
@@ -1,12 +1,13 @@
 package org.dimensinfin.poc.staticserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Collections;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+//@SpringBootApplication
 public class StaticserverApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
