1e6a02f7cdd10c760c7cfef9a1bb8ed629db1391
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-03 16:01:56 +0200

    -[POC.STATIC] Added the /api end point and one static content page with the minimal configuration required.
    Former-commit-id: 3ca05605daa8d65ec609f90a631a554ef25f8aa0

---
+++ POCs/static/src/main/java/org/dimensinfin/poc/staticserver/StaticserverApplication.java
@@ -0,0 +1,21 @@
+package org.dimensinfin.poc.staticserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Collections;
+
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class StaticserverApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(StaticserverApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8091"));
+        app.run(args);
+    }
+
+}
