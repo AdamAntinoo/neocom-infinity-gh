db7e4b0414b71fae5afd21c01953ebe87b1dc95c
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-07 23:30:01 +0200

    -[POC.STATIC] Removed unused project. -[PORTAL] Removed project because had no content and probably the project name will be changed. -[POC.FRONTEND] Added code to call the backend and continue with the login authorization process.
    Former-commit-id: af53c85bd68abf6f4cd0bac6f01901240ddea86b

--- POCs/frontend/src/app/login-validation/login-validation.component.ts
+++ POCs/frontend/src/app/login-validation/login-validation.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BackendService } from '../services/backend.service';
+import { AppStoreService } from '../services/app.store.service';
+//--- SERVICES
+// import { AppModelStoreService } from '../../services/app-model-store.service';
+// import { AppStoreService } from 'app/services/app-store.service';
+// import { BackendService } from 'app/services/backend.service';
 
 @Component({
   selector: 'app-login-validation',
@@ -9,19 +15,32 @@ import { ActivatedRoute } from '@angular/router';
 export class LoginValidationComponent implements OnInit {
   param1: string;
   param2: string;
-  constructor(private route: ActivatedRoute) {
+  constructor(protected appModelStore: AppStoreService,
+    protected backendService: BackendService,
+    protected route: ActivatedRoute) {
     console.log('>LoginValidation');
     this.route.queryParams.subscribe(params => {
       this.param1 = params['code'];
       this.param2 = params['state'];
     });
-    console.log('Parameters: '+'code='+this.param1);
-    console.log('Parameters: '+'state='+this.param2);
+    console.log('Parameters: ' + 'code=' + this.param1);
+    console.log('Parameters: ' + 'state=' + this.param2);
   }
 
   ngOnInit() {
     // Read the request parameters and check the caller is verified.
-
+    console.log(">>[ValidateAuthorizationPageComponent.ngOnInit]");
+    // Read the code to be interchanged from the URL query string.
+    this.route.queryParams
+      .subscribe(params => {
+        let code = params['code'];
+        // If the code is valid then generate the basic authorization and transfer the encoded data to the backend.
+        if (null != code) {
+          this.backendService.backendExchangeAuthorization(code);
+        } else {
+          // this.exception = true;
+        }
+      });
   }
 
 }
