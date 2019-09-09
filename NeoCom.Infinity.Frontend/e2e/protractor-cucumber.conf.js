da44301cd139fbba59cf6aac2cb4cb4f5604ccf5
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-09 15:12:43 +0200

    -[FRONTEND] Added test configuration from other project to configure Cucumber.
    Former-commit-id: d1aced03e04eba96f26d1280d744c38ad5363a27

---
+++ NeoCom.Infinity.Frontend/e2e/protractor-cucumber.conf.js
@@ -0,0 +1,47 @@
+// @ts-check
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+// const { SpecReporter } = require('jasmine-spec-reporter');
+const path = require('path');
+
+/**
+ * @type { import("protractor").Config }
+ */
+exports.config = {
+  allScriptsTimeout: 11000,
+  specs: [
+    './src/features/**/*.feature'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  cucumberOpts: {
+    require: ['./src/steps/**/*.steps.ts'],
+    format: 'json:.tmp/results.json',
+    strict: true
+  },
+  plugins: [{
+    package: 'protractor-multiple-cucumber-html-reporter-plugin',
+    options: {
+      // read the options part
+      automaticallyGenerateReport: true,
+      removeExistingJsonReportFile: true
+    }
+  }],
+  // jasmineNodeOpts: {
+  //   showColors: true,
+  //   defaultTimeoutInterval: 30000,
+  //   print: function() {}
+  // },
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.json')
+    });
+    // jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
+};
