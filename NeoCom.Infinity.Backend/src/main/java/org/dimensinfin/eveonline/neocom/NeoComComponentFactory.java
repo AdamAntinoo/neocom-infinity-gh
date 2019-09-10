01d3ad2c50f9aa68acb67d657b0581aa363fa952
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-10 17:04:39 +0200

    -[PROJECT] Upgraded configuration to compile project but still not running because of SB configuration.
    Former-commit-id: 78b9938032c0113a2adb5323fce7e6144cd38499

--- NeoCom.Infinity.Backend/src/main/java/org/dimensinfin/eveonline/neocom/NeoComComponentFactory.java
+++ NeoCom.Infinity.Backend/src/main/java/org/dimensinfin/eveonline/neocom/NeoComComponentFactory.java
@@ -1,4 +1,17 @@
 package org.dimensinfin.eveonline.neocom;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
 public class NeoComComponentFactory {
+    public ObjectMapper jsonMapper = new ObjectMapper();
+
+    public ObjectMapper getJsonMapper() {
+        if (null == this.jsonMapper) this.jsonMapper = new ObjectMapper();
+        Objects.requireNonNull(this.jsonMapper);
+        return this.jsonMapper;
+    }
 }
