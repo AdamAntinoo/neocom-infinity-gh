48baff21f528c4cd249f639c89cbba561d4c5d79
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-10 19:55:03 +0200

    -[REFACTOR] Rename the project and start to replace old code by the new Spring Boot code.
    Former-commit-id: 4eea387d08c79cdf6cd473fc5201fb79df27718c

--- NeoCom.Infinity.Backend/src/main/java/org/dimensinfin/eveonline/neocom/NeoComSession.java
+++ NeoCom.Infinity.Backend/src/main/java/org/dimensinfin/eveonline/neocom/NeoComSession.java
@@ -1,8 +1,19 @@
+//  PROJECT:     NeoCom.Microservices (NEOC.MS)
+//  AUTHORS:     Adam Antinoo - adamantinoo.git@gmail.com
+//  COPYRIGHT:   (c) 2017-2018 by Dimensinfin Industries, all rights reserved.
+//  ENVIRONMENT: Java 1.8 / SpringBoot-1.3.5 / Angular 5.0
+//  DESCRIPTION: This is the SpringBoot MicroServices module to run the backend services to complete the web
+//               application based on Angular+SB. This is the web version for the NeoCom Android native
+//               application. Most of the source code is common to both platforms and this module includes
+//               the source for the specific functionality for the backend services.
 package org.dimensinfin.eveonline.neocom;
 
-import org.dimensinfin.eveonline.neocom.database.entities.Credential;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.dimensinfin.eveonline.neocom.entities.Colony;
+import org.dimensinfin.eveonline.neocom.entities.Credential;
+import org.dimensinfin.eveonline.neocom.entities.Property;
+import org.dimensinfin.eveonline.neocom.entities.Job;
 
 /**
  * @author Adam Antinoo
@@ -56,3 +67,6 @@ public class NeoComSession {
 				.toString();
 	}
 }
+
+// - UNUSED CODE ............................................................................................
+//[01]
