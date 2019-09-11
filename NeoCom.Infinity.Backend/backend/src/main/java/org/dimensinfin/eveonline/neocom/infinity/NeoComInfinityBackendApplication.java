75101f735225bb873f6404a4cd5e6ead469aa03d
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-11 15:55:44 +0200

    -[BUG] Fixed the runtime problem. It is related to the DataManagement library that seems not to be compatible with the springboot configuration.
    Former-commit-id: e9941690d48c9af26aad3d14e3b6317fb57aeca0

--- NeoCom.Infinity.Backend/backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/NeoComInfinityBackendApplication.java
+++ NeoCom.Infinity.Backend/backend/src/main/java/org/dimensinfin/eveonline/neocom/infinity/NeoComInfinityBackendApplication.java
@@ -1,13 +1,27 @@
 package org.dimensinfin.eveonline.neocom.infinity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Objects;
+
+//@EnableScheduling
+//@EnableJpaAuditing
+//@Configuration(value="NeoComInfinityBackendApplication")
 @SpringBootApplication
 public class NeoComInfinityBackendApplication {
+    private static final Logger logger = LoggerFactory.getLogger(NeoComInfinityBackendApplication.class);
+    private static final NeoComComponentFactory neocomComponentFactory = new NeoComComponentFactory();
 
-	public static void main(String[] args) {
-		SpringApplication.run(NeoComInfinityBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+		logger.info(">> [NeoComInfinityBackendApplication.main]");
+		logger.info(">> [NeoComInfinityBackendApplication.main]> Check application startup...");
+		Objects.requireNonNull(neocomComponentFactory);
+        SpringApplication.run(NeoComInfinityBackendApplication.class, args);
+        logger.info("<< [NeoComInfinityBackendApplication.main]");
+    }
 }
