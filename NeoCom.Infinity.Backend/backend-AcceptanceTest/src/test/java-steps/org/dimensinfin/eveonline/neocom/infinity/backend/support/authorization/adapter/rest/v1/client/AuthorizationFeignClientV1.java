05f590921d79b6723fcb708445d0c6c602018898
Adam Antinoo <adamantinoo.git@gmail.com>
2019-09-14 15:44:52 +0200

    -[ACCEPTANCE] Create Feign client to access the application services and to encapsulate the request data.
    Former-commit-id: 6d15f1a26c1435d41abfddc63559666f4c2f9e7c

--- NeoCom.Infinity.Backend/backend-AcceptanceTest/src/test/java-steps/org/dimensinfin/eveonline/neocom/infinity/backend/support/authorization/adapter/rest/v1/client/AuthorizationFeignClientV1.java
+++ NeoCom.Infinity.Backend/backend-AcceptanceTest/src/test/java-steps/org/dimensinfin/eveonline/neocom/infinity/backend/support/authorization/adapter/rest/v1/client/AuthorizationFeignClientV1.java
@@ -1,12 +1,22 @@
-package org.dimensinfin.eveonline.neocom.infinity.backend.support.authentication.adapter.converters;
+package org.dimensinfin.eveonline.neocom.infinity.backend.support.authorization.adapter.rest.v1.client;
+
+import org.dimensinfin.eveonline.neocom.infinity.authorization.rest.dto.ValidateAuthorizationTokenRequest;
+import org.dimensinfin.eveonline.neocom.infinity.authorization.rest.dto.ValidateAuthorizationTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @FeignClient(
-        name = "cards-support",
-        url = "${orangebank.client.cards.url}",
-        primary = false,
-        configuration = InternalFeignClientConfiguration.class)
-public interface CardsFeignClientV1 {
-
-    @PostMapping(path = "/v1/cards-agreements")
-    ResponseEntity<CreateCardAgreementResponse> createCardAgreement(@RequestBody @Valid CreateCardAgreementRequest createCardAgreementRequest);
+		name = "authorization",
+		url = "${orangebank.client.cards.url}",
+		primary = false/*,
+        configuration = InternalFeignClientConfiguration.class*/)
+public interface AuthorizationFeignClientV1 {
+	@GetMapping(path = "/validateAuthorizationToken/{code}/state/{state}")
+	ResponseEntity<ValidateAuthorizationTokenResponse> validateAuthorizationToken( @PathVariable final String code,
+	                                                                               @PathVariable final String state,
+	                                                                               @PathVariable("dataSource") final Optional<String> dataSource );
 }
