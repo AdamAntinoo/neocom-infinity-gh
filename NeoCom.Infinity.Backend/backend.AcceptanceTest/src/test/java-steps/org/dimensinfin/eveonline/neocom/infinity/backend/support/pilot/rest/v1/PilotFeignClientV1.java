be8f1d2e3b40b3dfb7e1fc0de6331b0d9d6a42b6
Adam Antinoo <adamantinoo.git@gmail.com>
2019-10-12 15:55:43 +0200

    -[REFACTOR] Reorganize the acceptance support source code.
    Former-commit-id: eee695d82f05a012e63365acebc02997066e5bc5

--- NeoCom.Infinity.Backend/backend.AcceptanceTest/src/test/java-steps/org/dimensinfin/eveonline/neocom/infinity/backend/support/pilot/rest/v1/PilotFeignClientV1.java
+++ NeoCom.Infinity.Backend/backend.AcceptanceTest/src/test/java-steps/org/dimensinfin/eveonline/neocom/infinity/backend/support/pilot/rest/v1/PilotFeignClientV1.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import org.dimensinfin.eveonline.neocom.infinity.backend.support.NeoComApiv1;
+import org.dimensinfin.eveonline.neocom.infinity.pilot.client.v1.PilotDataResponse;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -16,7 +17,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 @Component
 public class PilotFeignClientV1 {
 	public ResponseEntity<PilotDataResponse> getPilotData( final Integer pilotIdentifier,
-	                                                             final String authorizationToken ) throws IOException {
+	                                                       final String authorizationToken ) throws IOException {
 		final NeoComApiv1 serviceGetAccessToken = new Retrofit.Builder()
 				.baseUrl( NeoComApiv1.NEOCOM_BACKEND_APP_HOST )
 				.addConverterFactory( JacksonConverterFactory.create() )
