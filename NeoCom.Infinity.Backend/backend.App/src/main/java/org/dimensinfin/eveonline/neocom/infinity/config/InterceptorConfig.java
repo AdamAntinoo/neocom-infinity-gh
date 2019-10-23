40df1e9446bae8e804b112797bdb5cca630aa544
Adam Antinoo <adamantinoo.git@gmail.com>
2019-10-23 18:16:56 +0200

    -[BACKEND] Aplication readjust to remove the CORS configuration problem. Step 03. The Interceptor related to additional headers was the problem.
    Former-commit-id: 6b6a920648190771d0067e0145b1c27f5166c4b5

--- NeoCom.Infinity.Backend/backend.App/src/main/java/org/dimensinfin/eveonline/neocom/infinity/config/InterceptorConfig.java
+++ NeoCom.Infinity.Backend/backend.App/src/main/java/org/dimensinfin/eveonline/neocom/infinity/config/InterceptorConfig.java
@@ -1,16 +1,16 @@
 package org.dimensinfin.eveonline.neocom.infinity.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import org.dimensinfin.eveonline.neocom.infinity.config.interceptor.HeaderResponseInterceptor;
 import org.dimensinfin.eveonline.neocom.infinity.config.interceptor.LogRequestHeaderInterceptor;
 import org.dimensinfin.eveonline.neocom.infinity.config.interceptor.LogRequestInterceptor;
 import org.dimensinfin.eveonline.neocom.infinity.config.interceptor.LogResponseInterceptor;
 import org.dimensinfin.eveonline.neocom.infinity.config.interceptor.LogTimingInterceptor;
 
-//@Configuration
+@Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 	@Value("${P.runtime.logging.request}")
 	private String logRequestFlag = "true";
@@ -29,9 +29,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
 			registry.addInterceptor( new LogResponseInterceptor() ).addPathPatterns( "/api/**" );
 		if (logTimingFlag.equalsIgnoreCase( "true" ))
 			registry.addInterceptor( new LogTimingInterceptor() ).addPathPatterns( "/api/**" );
-//		if ( logMetricsFlag.equalsIgnoreCase( "true" ))
-//		registry.addInterceptor(new MetricsInterceptor()).addPathPatterns("/api/**");
+////		if ( logMetricsFlag.equalsIgnoreCase( "true" ))
+////		registry.addInterceptor(new MetricsInterceptor()).addPathPatterns("/api/**");
 		registry.addInterceptor( new LogRequestHeaderInterceptor() ).addPathPatterns( "/api/**" );
-		registry.addInterceptor( new HeaderResponseInterceptor() ).addPathPatterns( "/api/**" );
+//		registry.addInterceptor( new HeaderResponseInterceptor() ).addPathPatterns( "/api/**" );
 	}
 }
