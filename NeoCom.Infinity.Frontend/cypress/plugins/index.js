2abff877132e69c05d1784be886b8fbf33a8225b
Adam Antinoo <adamantinoo.git@gmail.com>
2020-01-19 19:31:37 +0100

    -[ACCEPTANCE] Adjusting the Cypress configuration.
    Former-commit-id: 29a3d64e859cfe9af01f72adeb88f97f9769f3b1

--- NeoCom.Infinity.Frontend/cypress/plugins/index.js
+++ NeoCom.Infinity.Frontend/cypress/plugins/index.js
@@ -1,7 +1,8 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
-const cucumber = require('cypress-cucumber-preprocessor').default
+const webpack = require("@cypress/webpack-preprocessor");
 
-module.exports = (on, config) => {
-    // on('file:preprocessor', cypressTypeScriptPreprocessor);
-    on('file:preprocessor', cucumber())
-}
+module.exports = on => {
+    const options = {
+        webpackOptions: require("../webpack.config.js")
+    };
+    on("file:preprocessor", webpack(options));
+};
