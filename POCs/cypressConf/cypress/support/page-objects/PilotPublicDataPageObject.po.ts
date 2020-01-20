1d14b36173a55d8eaddaefbdb4fd051ebe3b1343
Adam Antinoo <adamantinoo.git@gmail.com>
2020-01-20 16:52:26 +0100

    -[POC] Adding the typescript support from scratch using module.
    Former-commit-id: ce484c6d7da802082c0547b6b517058d05ef1215

--- POCs/cypressConf/cypress/support/page-objects/PilotPublicDataPageObject.po.ts
+++
@@ -1,7 +0,0 @@
-const PILOT_HEADER = 'pilot-header';
-
-export class PilotPublicDataPageObject {
-    public checkPilotHeader(target: string): void {
-        cy.get('#pilot-header').contains(target);
-    }
-}
